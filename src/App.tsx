2933e4de3b4834c0e96426dba7f10ac0297d069d
omrawat23 <omrawat23@gmail.com>
2024-12-26 03:54:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+export default function App() {
+  return (
+    <div className='flex flex-col'>
+     omi
+     <Button>omi</Button>
+    </div>
+  )
+}
